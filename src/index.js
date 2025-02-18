1b547e82eed92993a3086bba99ac10b60e6fcd8a
project-demo <kumarikomal62057@gmail.com>
2025-02-18 23:31:25 +0530

    component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,78 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
 function App() {
-  return <h1>Pizza Menugit</h1>;
+  return (
+    <div>
+      <h1>Pizza Menugit</h1>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast react Pizza Co.</h1>;
+}
+
+function Menu() {}
+
+function Footer() {}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="spinaci.jpg" />
+      <h2>Pizza spinaci</h2>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </div>
+  );
 }
 // for React v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
